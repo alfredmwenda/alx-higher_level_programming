e944ab90b609e7478afe34d1604581634e08c924
root <alfredmwenda>
2022-06-08 06:57:40 -0700

    .

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum, difference, product, quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
+    """import add_o fn if in main and print sum of 1 and 2"""
+    from add_0 import add
 
-    a = 10
-    b = 5
-
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
